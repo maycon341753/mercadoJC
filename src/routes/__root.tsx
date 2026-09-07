7bc8a8fd2e25e8ed4c242d3726f4f8acab1cafd3
Maycon Wender <maycon341753@gmail.com>
2026-09-07 08:59:37 -0300

    chore: troca favicon para carrinho kawaii (favicon2.png)

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -73,7 +73,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     ],
     links: [
       { rel: "stylesheet", href: appCss },
-      { rel: "icon", href: "/favicon.ico", type: "image/x-icon" },
+      { rel: "icon", href: "/favicon2.png", type: "image/png" },
       { rel: "manifest", href: "/manifest.webmanifest" },
     ],
   }),
